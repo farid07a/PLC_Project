203d5a99e5cf4c0a2c742df77e2a16256a0dcca1
faridPC <faridmaster02@gmail.com>
2022-10-12 17:04:18 +0200

    upload My Project Some Code python connection attributs

--- Model/PLC.py
+++ Model/PLC.py
@@ -2,6 +2,7 @@ import MySQLdb
 from snap7 import client
 import mysql.connector
 from PracticePython import ConnectionDB
+from data_type import DataType
 class plcMachine :
 
     connection_db = ConnectionDB.ConnectionDB()
@@ -14,6 +15,7 @@ class plcMachine :
     ModelPlc = ""
     clientPlc = client.Client()
     ConnectionObj=False
+    test_var_connection=mysql.connector.connect()
 
     def __init__(self,IdPlc, IP, RACK, SLOT ):
         self.IdPlc = IdPlc
@@ -39,6 +41,7 @@ class plcMachine :
                 self.ConnectionObj=True
         except Exception as e:
             print("Occured Exception")
+
 
     def get_client_plc(self):
         return self.clientPlc
@@ -71,7 +74,7 @@ class plcMachine :
     def getRACK(self):
         return self.RACK
 
-    def AddNewPLC(self):
+    def add_new_plc(self):
         Query = "INSERT INTO plc_controller (Model,IP,RACK,	SLOT) values (%s,%s,%s,%s)"
         try:
             cnx=mysql.connector.connect()
@@ -90,7 +93,8 @@ class plcMachine :
 
     def get_list_plc(self):
         query = "select * from plc_controller"
-        cnx = None
+        cnx = mysql.connector.connect()
+
         list_plc = []
         try:
             cnx = mysql.connector.connect(host='localhost', user='root', passwd='', database='test')
@@ -115,6 +119,44 @@ class plcMachine :
         for plc in list_plc:
             print("|", plc[0] ,"\t |", plc[1], "\t |", plc[2], "\t \t |",plc[3],"\t |",plc[4] ,"\t|")
 
+    def add_new_colum_in_plc_table(self, name,value):
+        datatype_obj = DataType()
+
+        try:
+            connection = mysql.connector.connect(user='root', passwd='', host='localhost', database='test')
+        except:
+            print("Error: Can't connect to database")
+            return
+        dt_type=''
+        if type(value) ==int:
+            dt_type = datatype_obj.INT
+        elif type(value)== float:
+            dt_type=datatype_obj.FLOAT
+        elif type(value)== str:
+            dt_type = datatype_obj.VARCHAR
+        elif type(value)== bool:
+            dt_type = datatype_obj.BOOL
+        elif type(value) == bytearray:
+            dt_type=datatype_obj.BINARY
+        else:
+            print("Your value is not availble in my data_type class")
+            return
+
+
+        cursor = connection.cursor()
+        sql = 'ALTER TABLE plc ADD COLUMN '+ name +' VARCHAR(50)'
+        try:
+            cursor.execute(sql)
+            connection.commit()
+        except:
+            print("Error: unable to delete a record")
+            connection.rollback()
+        print(cursor.rowcount, 'record(s) deleted')
+        cursor.close()
+        connection.close()
+
+    def test_connecttion(self):
+
 
     def menu(self):
         flag = True
@@ -122,7 +164,7 @@ class plcMachine :
             print(" Menu :\n 1: Add New PLC \n 2: Connecting To Plc \n 3 :Add Colum To Table PLC \n 4: Exit \n 5 display list plc")
             x = int(input(" Enter your Choice:\n"))
             if x == 1:
-                self.AddNewPLC();
+                self.add_new_plc();
                 # flag = False
 
             elif x == 2:
@@ -130,6 +172,8 @@ class plcMachine :
             elif x == 3:
                 print("Add New Colum In PLC Data")
                 self.get_list_plc()
+
+
             elif x == 4:
                 print("Close Menu")
                 flag = False
---
+++ Model/data_type.py
@@ -0,0 +1,33 @@
+
+class DataType:
+    VARCHAR = "VARCHAR(50)"
+    BINARY = "BINARY"
+    BIT = "BIT"
+    BOOL = "BOOL"
+    INT = "INT"
+    FLOAT = "FLOAT"
+    def __init__(self):
+        VARCHAR = "VARCHAR(50)"
+        BINARY = "BINARY"
+        BIT = "BIT"
+        BOOL = "BOOL"
+        INT = "INT"
+        FLOAT = "FLOAT"
+
+    def get_int(self):
+        return self.INT
+
+    def get_binary(self):
+        return self.BINARY
+
+    def get_bit(self):
+        return self.BIT
+
+    def get_bool(self):
+        return self.BOOL
+
+    def get_varchar(self):
+        return self.VARCHAR
+
+    def get_float(self):
+        return self.FLOAT

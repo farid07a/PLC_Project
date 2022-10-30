f5845060bd1af2c11f9844be537ed9db04daf598
farid <farid07a>
2022-10-30 17:04:44 +0100

    Connection to SQL Server

--- GUI_Practice/subclass_main_window.py
+++ GUI_Practice/subclass_main_window.py
@@ -268,7 +268,7 @@ class MainWindow(QMainWindow):
                   self.plc_object.getSlot())
 
     def start_reading_data(self):
-
+        row_line=0
         while self.stat_read_data:
             try:
                 self.plc_physic.connect(self.plc_object.getIP(), self.plc_object.getRACK(), self.plc_object.getSlot())
@@ -296,37 +296,44 @@ class MainWindow(QMainWindow):
             self.read_data_obj.Data_Input = db  # set dataBloc to DataInput to save with operation
             print(self.read_data_obj.Data_Input)
             self.read_data_obj.insert_input_data()  # save The operation read of data with primary key
+            id_read = self.read_data_obj.get_last_operation_read()  # return last id of Operation Read
             list_tags = self.tag_obj.list_of_tags_by_id_plc(self.plc_object.getIdPlc())  # get list of tags to get id
-            for tag_i in list_tags:
 
+
+            for tag_i in list_tags:
                 id_tag = tag_i.get_id_tag()
                 data_type = tag_i.get_data_type()
                 addres_byte = tag_i.get_address_start_byte()
-
                 print("ID_tag:", id_tag, "data_type:", data_type, "addres_byte:", addres_byte, "End_adres_byte",
                       addres_byte + 2)
                 part_of_tag = bytearray(4)
-
+                value =0
                 if data_type == "int":
                     part_of_tag = db[addres_byte:addres_byte + 2]
                     print("part of package from db :", part_of_tag)
+                    value=get_int(db, addres_byte)
                     print(get_int(db, addres_byte))
 
                 elif data_type == "real":
                     part_of_tag = db[addres_byte:addres_byte + 4]
                     print(get_int(db, addres_byte))
+                    value = get_int(db, addres_byte)
+                    value = round(value, 1)
 
                 elif data_type == "bool":
                     addres_bit = tag_i.get_address_start_bit()
                     part_of_tag = db[addres_byte:addres_byte + 1]
                     print(snap7.util.get_bool(db, addres_byte, addres_bit))
+                    value = snap7.util.get_bool(db, addres_byte, addres_bit)
 
-                id_read = self.read_data_obj.get_last_operation_read()  # return last id of Operation Read
-                print("Last ID of Operationread :", id_read)
+                print("Last ID of Operation read :", id_read)
                 self.split_data_tag.create(id_tag, id_read, part_of_tag)  #
                 self.split_data_tag.insert_split_data_package_database()  # insert data in database
 
+
                 self.display_history_with_update_query()
+
+
             sleep(3)
 
     def start_reading_data_without_while(self):
